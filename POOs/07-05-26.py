54bfbc698c842980f00160da6c956b8c44f73e9a
Andre Luiz <andr234ibatera@gmail.com>
2026-05-12 18:46:56 -0300

    Realizando a correção dos elementos de continuidades
    Agora kx, kzt e kzl podem ser herdados de outro meio

--- POOs/07-05-26.py
+++ POOs/07-05-26.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from typing import Union
+from typing import Union, Optional
 from abc import ABC
 from enum import Enum
 
@@ -15,12 +15,17 @@ class Medium(ABC):
             rho (float): The `mass per unit volume` in Kg/m**3 of the medium through which the wave travels. (Confirm information with the group)
         """
         super().__init__()
-        self.vp = vp
-        self.vs = vs
-        self.rho = rho
+        self.vp: float = vp
+        self.vs: float = vs
+        self.rho: float = rho
         # Deslocation Field
-        self._theta = None
-        self._omega = None
+        self._theta: float = None
+        self._omega: float = None
+        # Continuit Values
+        self._k_x: float = None
+        self._k_zt: Union[np.float64, np.complex128] = None
+        self._k_zl: Union[np.float64, np.complex128] = None
+        
         
     @property
     def lame_mu(self) -> float:
@@ -57,7 +62,6 @@ class Medium(ABC):
         """
         return self._theta
     
-    # TODO o grau só varia de 0 a 90
     @theta.setter
     def theta(self, angle) -> None:
         """Setter of the `theta` property
@@ -65,7 +69,7 @@ class Medium(ABC):
         Args:
             angle (float, optional): Angle of incidence of the wave in degrees.
         """
-        assert angle>=0.0 and angle<=360.0, "angle is an invalid value, has to be from 0 to 360"
+        assert angle>=0.0 and angle<=90.0, "angle is an invalid value, has to be from 0 to 90"
         self._theta = (angle*np.pi)/180
     
     @property
@@ -97,13 +101,22 @@ class Medium(ABC):
         return self.omega/self.vp
     
     @property
-    def k_x(self) -> float: #TODO Precisa forçar que o kx possa ser passado diretamente, pois precisa garantir continuidade
+    def k_x(self) -> float:
         """_summary_ Get definitions with the group
 
         Returns:
             float: _description_
         """
-        return self.k_l*np.sin(self.theta)
+        return self._k_x if self._k_x else self.k_l*np.sin(self.theta)
+    
+    @k_x.setter
+    def k_x(self, inherited: float) -> None:
+        """_summary_
+
+        Args:
+            inherited (Union[np.float64, np.complex128]): _description_
+        """
+        self._k_x = inherited
     
     @property
     def k_t(self) -> float:
@@ -115,29 +128,46 @@ class Medium(ABC):
         return self.omega/self.vs
     
     @property
-    def k_zt(self) -> Union[np.float64, np.complex128]:#TODO Precisa forçar que o k_zt possa ser passado diretamente, pois precisa garantir continuidade
+    def k_zt(self) -> Union[np.float64, np.complex128]:
         """_summary_ Get definitions with the group
 
         Returns:
             Union[np.float64, np.complex128]: _description_
         """
-        return np.emath.sqrt((self.k_t**2) - (self.k_x**2))
+        return self._k_zt if self._k_zt else np.emath.sqrt((self.k_t**2) - (self.k_x**2))
+    
+    @k_zt.setter
+    def k_zt(self, inherited: Union[np.float64, np.complex128]) -> None:
+        """_summary_
+
+        Args:
+            inherited (Union[np.float64, np.complex128]): _description_
+        """
+        self._k_zt = inherited
     
     @property
-    def k_zl(self) -> Union[np.float64, np.complex128]:#TODO Precisa forçar que o k_zl possa ser passado diretamente, pois precisa garantir continuidade
+    def k_zl(self) -> Union[np.float64, np.complex128]:
         """_summary_  Get definitions with the group
 
         Returns:
             Union[np.float64, np.complex128]: _description_
         """
-        return np.emath.sqrt((self.k_l**2) - (self.k_x**2))
+        return self._k_zl if self._k_zl else np.emath.sqrt((self.k_l**2) - (self.k_x**2))
+    
+    @k_zl.setter
+    def k_zl(self, inherited: Union[np.float64, np.complex128]) -> None:
+        """_summary_
+
+        Args:
+            inherited (Union[np.float64, np.complex128]): _description_
+        """
+        self._k_zl = inherited
     
     def __str__(self):
         return f"\t\tMaterial Specifications\nVp={self.vp}\tVs={self.vs}\tRho={self.rho}\n\t\tLamé Constants\nLambda={self.lame_lambda}\tMu={self.lame_mu}\n\t\tDisplacement Fields\nTheta={self.theta}\tOmega={self.omega}\n\t\tKs\nKl={self.k_l}\tKx={self.k_x}\tKt={self.k_t}\tKzt={self.k_zt}\tKzl={self.k_zl}"
 
 
 # TODO confirmar com o professor o nome correto em inglês
-# TODO qual o limiar de theta que define uma onda indo pra cima e para baixo? Entre 0 e 180 é considerado cima e 180 e 360 considerado baixo? Ou existe uma "zona morta" próximo a 180 e 360?
 class DisplacementPolarizationMatriz(ABC):
     def __init__(self, medium: Medium):
         super().__init__()
@@ -178,6 +208,7 @@ if __name__ == "__main__":
     medium_2 = Medium(vp=2000.0, vs=1500.0, rho=5000.0)
     medium_2.theta = 30.0
     medium_2.omega = 100.0
+    medium_2.k_x = medium_1.k_x
     # Displacement Matriz fom the botton
     disp_1 = DisplacementPolarizationMatriz(medium=medium_1)
     # Displacement Matriz fom the top
